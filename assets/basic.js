15938d9a09e9101e84c6a2cbbceb4b8994a8928b
NickCorl <nicholas.corl@gmail.com>
2017-08-08 02:28:55 -0600

    mionor tweeks to footer, fixed nav bar to now save when closed bby enabling hovering

--- assets/basic.js
+++ assets/basic.js
@@ -66,24 +66,3 @@ $(document).ready(function(){
         }
     );
 });
-
-
-     // Get the modal
-     var modal = document.getElementById('id01');
-
-     // When the user clicks anywhere outside of the modal, close it
-     window.onclick = function(event) {
-       if (event.target == modal) {
-         modal.style.display = "none";
-       }
-     }
-
-
-
-     $(document).ready(function() {
-       $('.dropdown-submenu a.test').on("click", function(e) {
-         $(this).next('ul').toggle();
-         e.stopPropagation();
-         e.preventDefault();
-       });
-     });
